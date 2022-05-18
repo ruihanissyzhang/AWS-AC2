cc07e6f0b6bfcf6fe9ccf5379506196fb83dcf14
ruihanissyzhang <71948029+ruihanissyzhang@users.noreply.github.com>
2022-05-18 15:18:20 -0400

    Update variables.tf
    The purpose of this file is to define a new instance name.

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable "instance_name" {
   description = "Value of the Name tag for the EC2 instance"
+  # the type of the variable is string
   type        = string
   default     = "ExampleAppServerInstance"
 }
