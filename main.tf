5b4b3168e17f540b93c4e274e27a59aacf21cffd
ruihanissyzhang <71948029+ruihanissyzhang@users.noreply.github.com>
2022-05-18 15:14:44 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,20 +1,24 @@
 terraform {
   required_providers {
     aws = {
+      #provider's source
       source  = "hashicorp/aws"
       version = "~> 3.27"
     }
   }
-
+  #the version must greater than 0.14.9
   required_version = ">= 0.14.9"
 }
 
+#the provider is AWS in this case, Terraform uses the provider to create and manage the resources
 provider "aws" {
   profile = "default"
   region  = "us-west-2"
 }
 
+# resource type is aws_instance and resource name is app_server
 resource "aws_instance" "app_server" {
+  # I changed the ami id when I want to change the aws_instance.app_server resource under the provider block 
   ami           = "ami-08d70e59c07c61a3a"
   instance_type = "t2.micro"
 
