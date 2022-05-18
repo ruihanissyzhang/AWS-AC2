71ac1cfbbbdbff82d750a0ce510b21dda97885ce
ruihanissyzhang <71948029+ruihanissyzhang@users.noreply.github.com>
2022-05-18 15:22:03 -0400

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,8 +1,10 @@
+# define outputs for my EC2 instance's ID
 output "instance_id" {
   description = "ID of the EC2 instance"
   value       = aws_instance.app_server.id
 }
 
+# define outputs for my EC2 instance's IP address
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
   value       = aws_instance.app_server.public_ip
